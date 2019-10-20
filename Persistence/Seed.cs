4e608a008449426c23928ab0d6c5210a6722bd92
rajanshrestha <shrestr1@gmatc.matc.edu>
2019-10-20 17:35:57 -0500

    Completed Activity 5

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -16,17 +16,17 @@ namespace Persistence
                     new Post {
                         Title = "First post!",
                         Date = DateTime.Now.AddDays(-10),
-                        Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+                        Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     },
                     new Post {
                         Title = "This is my second post!",
                         Date = DateTime.Now.AddDays(-7),
-                        Body = "Sed ut perspicciatis unde omnis iste natus error sit voluptatem accusantium dolor"
+                        Body = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
                     },
                     new Post {
                         Title = "Another day, another post!",
                         Date = DateTime.Now.AddDays(-4),
-                        Body = "But I must explain to you how all this mistaken idea of denouncing pleasure"
+                        Body = "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
                     }
                     };
 
