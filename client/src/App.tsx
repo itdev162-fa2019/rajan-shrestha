a9e219843e80c16c703c73e90668e406d1529cac
rajanshrestha <shrestr1@gmatc.matc.edu>
2019-12-12 22:38:11 -0600

    Completed Activity 9

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,111 +1,56 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import PostList from './components/PostList/PostList';
-import Post from './components/Post/Post';
-import './App.css';
-import CreatePost from './components/Post/CreatePost';
-import EditPost from './components/Post/EditPost';
-import axios from 'axios';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PostList from "./components/PostList/PostList";
+import Post from "./components/Post/Post";
+import "./App.css";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     posts: [],
     post: null
-  }
+  };
 
   componentDidMount() {
-    axios.get('http://localhost:5000/api/posts')
-      .then((response) => {
+    axios
+      .get("http://localhost:5000/api/posts")
+      .then(response => {
         this.setState({
           posts: response.data
-        })
+        });
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(`Error fetching data: ${error}`);
-      })
+      });
   }
 
-  viewPost = (post) => {
+  viewPost = post => {
     console.log(`view ${post.title}`);
     this.setState({
       post: post
     });
-  }
+  };
 
   render() {
-    deletePost = post => {
-      axios
-        .delete(`http://localhost:5000/api/posts/${post.id}`)
-        .then(response => {
-          const newPosts = this.state.posts.filter(p => p.id !== post.id);
-          this.setState({
-            posts: [...newPosts]
-          });
-        })
-        .catch(error => {
-          console.error(`Error deleting post: ${error}`);
-        });
-    };
     const { posts, post } = this.state;
 
-    editPost = post => {
-      this.setState({
-        post: post
-      });
-    };
-
-    onPostCreated = post => {
-      const newPosts = [...this.state.posts, post];
-
-      this.setState({
-        posts: newPosts
-      });
-    };
-
-    onPostUpdated = post => {
-      console.log('updated post: ', post);
-      const newPosts = [...this.state.posts];
-      const index = newPosts.findIndex(p => p.id === post.id);
-
-      newPosts[index] = post;
-
-      this.setState({
-        posts: newPosts
-      });
-    };
-
-    render() {
-      const { posts, post } = this.state;
-    
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            BlogBox
-        </header>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/new-post">New Post</Link>
-        </nav>
+          <header className="App-header">BlogBox</header>
+
           <main className="App-content">
             <Switch>
               <Route exact path="/">
-                <PostList posts={posts} clickPost={this.viewPost} deletePost={this.deletePost} editPost={this.editPost}/>
+                <PostList posts={posts} clickPost={this.viewPost} />
               </Route>
               <Route path="/posts/:postId">
                 <Post post={post} />
               </Route>
-              <Route path="/new-post">
-                <CreatePost onPostCreated={this.onPostCreated} />
-              </Route>
-              <Route path="/edit-post/:PostId">
-                <EditPost post={post} onPostUpdated={this.onPostUpdated} />
-              </Route>
-            </Switch>            
+            </Switch>
           </main>
         </div>
       </Router>
-      
     );
   }
 }
--- client/src/components/PostList/PostListItem.js
+++ client/src/components/PostList/PostListItem.js
@@ -1,36 +1,25 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import slugify from 'slugify';
-import './styles.css'
+import React from "react";
+import { useHistory } from "react-router-dom";
+import slugify from "slugify";
+import "./styles.css";
 
 const PostListItem = props => {
-    const { post, clickPost, deletePost, editPost } = props;
-    const history = useHistory();
+  const { post, clickPost } = props;
+  const history = useHistory();
 
-    const handleClickPost = post => {
-        const slug = slugify(post.title, { lower: true});
+  const handleClickPost = post => {
+    const slug = slugify(post.title, { lower: true });
 
-        clickPost(post);
-        history.push(`/posts/${slug}`);
-    };
+    clickPost(post);
+    history.push(`/posts/${slug}`);
+  };
 
-    const handleEditPost = post => {
-        editPost(post);
-        history.push(`/edit-post/${post.id}`);
-    };
-
-    return (
-        <div>
-        <div className="postListItem" onClick={() => handleClickPost(post)}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-        </div>
-        <div className="postControls">
-            <button onClick={() => deletePost(post)}>Delete</button>
-                <button onClick={() =>handleEditPost(post)}>Edit</button>
-        </div>
-        </div>
-    );
+  return (
+    <div className="postListItem" onClick={() => handleClickPost(post)}>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+    </div>
+  );
 };
 
 export default PostListItem;
